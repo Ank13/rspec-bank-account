2443deaeb4809d0b644cf8bbc466116602984142
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-25 09:49:32 -0500

    first commit

--- account_spec.rb
+++ account_spec.rb
@@ -3,7 +3,22 @@ require "rspec"
 require_relative "account"
 
 describe Account do
+  let(:acct_number) {3123456789}
+  let(:account) { Account.new(acct_number) }
+
   describe "#initialize" do
+    context 'with valid input' do
+      it 'requires at least one argument' do
+        expect { Account.new }.to raise_error(ArgumentError)
+      end
+
+      it 'creates an Account with the account number' do
+        account.acct_number.should eq("******6789")
+      end
+
+    end
+
+
 
   end
 
